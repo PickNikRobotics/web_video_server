04baea514f0f76dd7a963f7e6900282f184e23f5
Giovanni Remigi <giovanni.remigi@picknik.ai>
2025-03-27 17:51:38 +0100

    Reviews.

--- include/web_video_server/image_streamer.h
+++ include/web_video_server/image_streamer.h
@@ -1,7 +1,7 @@
 #ifndef IMAGE_STREAMER_H_
 #define IMAGE_STREAMER_H_
 
-#include <rclcpp/rclcpp.hpp>
+#include "web_video_server/utils.h"
 #include <image_transport/image_transport.hpp>
 #include <image_transport/transport_hints.hpp>
 #include <opencv2/opencv.hpp>
@@ -62,6 +62,7 @@ protected:
   virtual void restreamFrame(double max_age);
   virtual void initialize(const cv::Mat &);
 
+
   image_transport::Subscriber image_sub_;
   int output_width_;
   int output_height_;
@@ -78,6 +79,38 @@ private:
   bool initialized_;
 
   void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg);
+
+  /**
+   * @brief Detects QoS profile settings used by publishers on a specified topic.
+   * 
+   * This function queries the ROS2 middleware to discover publishers on the 
+   * given topic and extracts their QoS profile settings. It's useful for 
+   * creating subscribers that automatically match the publisher's QoS.
+   * 
+   * @param nh The ROS2 node.
+   * @param topic The full name of the topic to query.
+   *
+   * @return The detected QoS profile if a publisher is found, std::nullopt otherwise
+   */
+  static std::optional<rmw_qos_profile_t> detect_publisher_qos(rclcpp::Node::SharedPtr nh,
+                                                               const std::string &topic);
+
+  /**
+   * @brief Get QoS profile based on user selection or auto-detect.
+   * 
+   * If the profile name is "auto" (the default one), this function queries the ROS2
+   * middleware to discover publishers on the given topic and extracts their QoS
+   * profile settings. Otherwise it returns a profile based on the given name.
+   * 
+   * @param nh The ROS2 node.
+   * @param profile_name The QoS profile name e.g. "auto" or "default".
+   * @param topic The full name of the topic to query.
+   *
+   * @return The detected QoS profile if a publisher is found, std::nullopt otherwise
+   */
+  static std::optional<rmw_qos_profile_t> get_qos_profile(rclcpp::Node::SharedPtr nh, 
+                                                          const std::string &profile_name, 
+                                                          const std::string& topic);
 };
 
 class ImageStreamerType
